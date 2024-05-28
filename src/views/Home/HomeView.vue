9b926f451797cd73331f47ad3ac550ae3437b767
jltdev15 <75570239+jltdev15@users.noreply.github.com>
2024-05-28 08:11:33 +0800

    add animation in components

--- src/views/Home/HomeView.vue
+++ src/views/Home/HomeView.vue
@@ -9,7 +9,11 @@ import FilterNavBar from '../../components/Navigation/FilterNavBar.vue'
     <div class="grid md:grid-cols-10">
       <FilterNavBar />
       <SideBar />
-      <router-view />
+      <router-view v-slot="slotProps">
+        <transition name="route" mode="out-in">
+          <component :is="slotProps.Component"></component>
+        </transition>
+      </router-view>
 
 
 
@@ -23,3 +27,30 @@ import FilterNavBar from '../../components/Navigation/FilterNavBar.vue'
 
   </base-container>
 </template>
+
+
+<style>
+.route-enter-from {
+  opacity: 0;
+  transform: translateX(-30px);
+}
+
+.route-leave-to {
+  opacity: 0;
+  transform: translateX(30px);
+}
+
+.route-enter-active {
+  transition: all 0.3s ease-out;
+}
+
+.route-leave-active {
+  transition: all 0.3s ease-in;
+}
+
+.route-enter-to,
+.route-leave-from {
+  opacity: 1;
+  transform: translateX(0);
+}
+</style>
